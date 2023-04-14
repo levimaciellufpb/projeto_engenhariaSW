90886593fb93ff5e5eb4c877013d580619842dc5
levimaciell <levimaciell1221@gmail.com>
2023-04-14 19:40:20 -0300

    Criação de classes de modelo e pequenas alterações

---
+++ src/model/Usuario.java
@@ -0,0 +1,25 @@
+package src.model;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Usuario {
+
+    private String email;
+    private String senha;
+    private List<Crianca> criancas;
+
+    public Usuario(String email, String senha){
+        //tratamento de erro básico
+        //Possivelmente validar email e validar senha
+        this.email = email;
+        this.senha = senha;
+        List<Crianca> criancas = new ArrayList<>();
+    }
+
+
+
+
+
+
+}
