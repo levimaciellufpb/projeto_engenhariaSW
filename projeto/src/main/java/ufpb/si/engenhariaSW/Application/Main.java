e952356bdd4de83ab87a21c22af781abc38dbe36
Jose Ryan Pereira da Silva <joseryan118@gmail.com>
2023-04-18 14:45:09 -0300

    Atualização da classe Telas

--- projeto/src/main/java/ufpb/si/engenhariaSW/Application/Main.java
+++ projeto/src/main/java/ufpb/si/engenhariaSW/Application/Main.java
@@ -5,9 +5,10 @@ import javax.swing.*;
 public class Main {
 
     public static void main(String[] args) {
-        JPasswordField passwordField = new JPasswordField();
 
+        JPasswordField passwordField = new JPasswordField();
         Telas.PrimeiraTela();
+
     }
 
 
--- projeto/src/main/java/ufpb/si/engenhariaSW/Application/Telas.java
+++ projeto/src/main/java/ufpb/si/engenhariaSW/Application/Telas.java
@@ -5,31 +5,40 @@ import javax.swing.*;
 public class Telas {
 
     private int escolha;
-    public static void PrimeiraTela (){
+
+    public static void PrimeiraTela() {
         Object[] opcoes = {"Login", "Cadastrar", "Sair"};
 
-        int opcaoSelecionada = JOptionPane.showOptionDialog(null, "Escolha uma opção", "Histórico de saúde", JOptionPane.DEFAULT_OPTION, JOptionPane.PLAIN_MESSAGE, null, opcoes, opcoes[0]);
-        if(opcaoSelecionada == 0){
-            LoginECadastro.FazerLogin();
-        } else if (opcaoSelecionada == 1) {
-            LoginECadastro.FazerCadastro();
-        } else if (opcaoSelecionada == 2) {
-            JOptionPane.showMessageDialog(null, "Até mais!");
+        boolean sair = false;
+
+        while (!sair) {
+            int opcaoSelecionada = JOptionPane.showOptionDialog(null, "Escolha uma opção", "Histórico de saúde", JOptionPane.DEFAULT_OPTION, JOptionPane.PLAIN_MESSAGE, null, opcoes, opcoes[0]);
+            if (opcaoSelecionada == 0) {
+                LoginECadastro.FazerLogin();
+                Telas.SegundaTela();
+            } else if (opcaoSelecionada == 1) {
+                LoginECadastro.FazerCadastro();
+            } else if (opcaoSelecionada == 2) {
+                JOptionPane.showMessageDialog(null, "Até mais!");
+                sair = true;
+            }
         }
 
+    }
 
+    public static void SegundaTela() {
+
+        Object[] opcoes1 = {"Doenças", "Medicações", "Dentições", "Consultas Médicas", "Alergias", "Outros", "Voltar"};
+
+        boolean sair = false;
+
+        while (!sair) {
+            int opcaoSelecionadaCadastro = JOptionPane.showOptionDialog(null, "Escolha uma opção", "Panel",
+                    JOptionPane.DEFAULT_OPTION, JOptionPane.PLAIN_MESSAGE, null, opcoes1, opcoes1[0]);
+            if (opcaoSelecionadaCadastro == 6) {
+                sair = true;
+            }
+        }
     }
 }
-
-
-
-
-
-//    public static void SegundaTela(){
-//
-//        Object[] opcoes1 = {"Doenças", "Medicações", "Dentições", "Consultas Médicas", "Alergias", "Outros", "Sair"};
-//        int opcaoSelecionadaCadastro = JOptionPane.showOptionDialog(null, "Escolha uma opção", "Panel",
-//                JOptionPane.DEFAULT_OPTION, JOptionPane.PLAIN_MESSAGE, null, opcoes1, opcoes1[0]);
-//    }
-
 
